8168e83695f2de5c96f595e12f29ec91475ae801
VinayPV <VinayPV@Vinay-PV>
2022-05-12 08:24:09 +0530

    Latest Code Updated

---
+++ src/test/java/page/EnterTimeTrackPage.java
@@ -0,0 +1,27 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class EnterTimeTrackPage {
+
+	public EnterTimeTrackPage(WebDriver driver) {
+		PageFactory.initElements(driver,this);
+	}
+	
+	public boolean verifyPageTitle(WebDriverWait wait,String eTitle) {
+		
+		try 
+		{
+			wait.until(ExpectedConditions.titleContains(eTitle));
+			return true;
+		}
+		catch (Exception e) 
+		{
+			return false;
+		
+		}
+	}
+}
